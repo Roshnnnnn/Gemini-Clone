c183a662de9d1a3ef5e135abc442a129cd87c139
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-03-13 00:22:12 +0530

    update sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,7 +5,12 @@ import { Context } from "../../context/Context";
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
-  const { onSent, prevPrompts, setRecentPrompt } = useContext(Context);
+  const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
   return (
     <div className="sidebar">
       <div className="top">
@@ -17,7 +22,7 @@ const Sidebar = () => {
           src={assets.menu_icon}
           alt=""
         />
-        <div className="new-chat">
+        <div onClick={() => newChat()} className="new-chat">
           <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
         </div>
@@ -25,9 +30,13 @@ const Sidebar = () => {
           <div className="recent">
             <p className="recent-title">Recent</p>
             {prevPrompts.map((item, index) => (
-              <div className="recent-entry" key={index}>
+              <div
+                onClick={() => loadPrompt(item)}
+                className="recent-entry"
+                key={index}
+              >
                 <img src={assets.message_icon} alt="" />
-                <p>{item}</p>
+                <p>{item.slice(0, 18)}</p>
               </div>
             ))}
           </div>
