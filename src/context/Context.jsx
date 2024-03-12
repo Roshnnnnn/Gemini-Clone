65020e3ad21bb342888e543b70da7f8dd46fdf30
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-03-12 23:54:56 +0530

    update contex

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -11,13 +11,35 @@ const ContextProvider = (props) => {
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
 
+  const delayPara = (index, nextWord) => {
+    setTimeout(() => {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
     setRecentPrompt(input);
     const response = await runChat(input);
-    setResultData(response);
+    setPrevPrompts((prev) => [...prev, input]);
+    let responseArray = response.split("**");
+    let newResponse;
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<br>" + responseArray[i] + "</br>";
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>");
+    // setResultData(newResponse2);
+    let newResponseArray = newResponse2.split(" ");
+    for (let i = 0; i < newResponseArray; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord + " ");
+    }
     setLoading(false);
     setInput("");
   };
